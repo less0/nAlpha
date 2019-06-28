5c08d1264fb4ac6058b39fdec91d20f1aba935bc
Paul K <kertscher@melag.de>
2019-06-28 12:18:19 +0200

    Added test case for #2

--- nAlpha.Test/TestFixture_AlphaShapeCalculator.cs
+++ nAlpha.Test/TestFixture_AlphaShapeCalculator.cs
@@ -7,6 +7,9 @@ using NUnit.Framework;
 
 namespace nAlpha.Test
 {
+    using System.Globalization;
+    using System.Text.RegularExpressions;
+
     [TestFixture]
     public class TestFixture_AlphaShapeCalculator
     {
@@ -73,6 +76,40 @@ namespace nAlpha.Test
             shapeCalculator.CloseShape = true;
             var sndShape = shapeCalculator.CalculateShape(pts);
             Assert.That(sndShape.Edges.Length, Is.GreaterThan(shape.Edges.Length));
+        }
+
+        [TestCase("0|0,1|1,0|1", 1, "0|0,1|1,0|1")]
+        [TestCase("0.1|0.1,0.9|0.1,0.9|0.9,0.8|0.9,0.8|0.2,0.1|0.2", 1.25, "0.1|0.1,0.9|0.1,0.9|0.9,0.8|0.9,0.8|0.2,0.1|0.2")]
+        public void CalculateAlphaShape_ContainsAllPoints(string inputPointsString, double alpha, string expectedResultingPointsString)
+        {
+            var inputPoints = ParsePoints(inputPointsString);
+            var expectedResultingPoints = ParsePoints(expectedResultingPointsString);
+
+            var shapeCalculator = new AlphaShapeCalculator();
+            shapeCalculator.Alpha = alpha;
+
+            var resultingShape = shapeCalculator.CalculateShape(inputPoints);
+            
+            Assert.That(resultingShape.Vertices, Is.EquivalentTo(expectedResultingPoints));
+        }
+
+        private Point[] ParsePoints(string stringRepresentation)
+        {
+            var singlePointStringRepresentations = stringRepresentation.Split(',');
+            return singlePointStringRepresentations.Select(this.ParsePoint).ToArray();
+        }
+
+        private Point ParsePoint(string stringRepresentation)
+        {
+            if (Regex.IsMatch(stringRepresentation, "^[0-9]+(\\.[0-9]+)?|[0-9]+(\\.[0-9]+)?"))
+            {
+                var indexOfSeparator = stringRepresentation.IndexOf('|');
+                var xCoordinate = double.Parse(stringRepresentation.Substring(0, indexOfSeparator), CultureInfo.InvariantCulture);
+                var yCoordinate = double.Parse(stringRepresentation.Substring(indexOfSeparator + 1), CultureInfo.InvariantCulture);
+                return new Point(xCoordinate, yCoordinate);
+            }
+
+            throw new Exception("Failed parsing the points");
         }
     }
 }
